a2cec32f5d350ea23bf2997402f0702540024768
pingping.zhang <1403280239@qq.com>
2020-08-30 09:44:15 +0800

    add

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 # coding:utf-8
 from flask import Flask
 import router
+import platform
 
 app = Flask(__name__)
 
 router.register(app)
 
 if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8041, processes=5)
     app.run()
 
+if __name__ == '__main__':
+    # 根据实际情况修改端口号
+    os_ = platform.platform()
+    if os_.startswith("Windows"):
+        app.run()
+    else:
+        app.run(host="0.0.0.0", port=8041, processes=5)
