33e5404a59858ef158b90e694dcc56b8a3c9bf57
pingping.zhang <1403280239@qq.com>
2020-08-31 08:49:31 +0800

    add

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ router.register(app)
 if __name__ == '__main__':
     # 根据实际情况修改端口号
     os_ = platform.platform()
-    if os_.startswith("Windows"):
-        app.run()
-    else:
-        app.run(host="0.0.0.0", port=8041, threaded=False, debug=True)
+    app.run()
+    # if os_.startswith("Windows"):
+    #     app.run()
+    # else:
+    #     app.run(host="0.0.0.0", port=8041, threaded=False, debug=True)
